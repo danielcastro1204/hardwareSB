ff97ae504cd248e548105c571ba322251338f8c2
danielcastro1204 <133542170+danielcastro1204@users.noreply.github.com>
2024-10-09 10:03:56 -0500

    Implementación MQTT

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,22 @@
 #include <SPIFFS.h>
 #include <ArduinoJson.h>
 #include <HTTPClient.h>
+#include <PubSubClient.h>
 
-const char* ssid = "gladys";
-const char* password = "gladysss";
+const char* ssid = "SILVIA_ESCOBAR";
+const char* password = "13303110";
 
-String url =  "http://192.168.249.173:8080/data/create";
+String url = "http://192.168.249.173:8080/data/create";
+
+const char* mqttServer = "broker.hivemq.com";  // Servidor MQTT
+const int mqttPort = 1883;                     // Puerto MQTT
+const char* clientName = "ESP32ClientIcesi";   // Nombre del cliente MQTT
+const char* topic = "icesitel/medicion";       // Topic para recibir comandos
 
 HTTPClient http;
 Adafruit_MPU6050 mpu;
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
 
 int samplingRateHz = 20;   
 int numSamples = 200;      
@@ -37,16 +45,15 @@ void sendDataToServer(float accX, float accY, float accZ, float gyroX, float gyr
   jsonDoc["xac"] = accX;     // Aceleración X
   jsonDoc["yac"] = accY;     // Aceleración Y
   jsonDoc["zac"] = accZ;     // Aceleración Z
-  jsonDoc["xgi"] = gyroX;  // Giroscopio X
-  jsonDoc["ygi"] = gyroY;  // Giroscopio Y
-  jsonDoc["zgi"] = gyroZ;  // Giroscopio Z
+  jsonDoc["xgi"] = gyroX;    // Giroscopio X
+  jsonDoc["ygi"] = gyroY;    // Giroscopio Y
+  jsonDoc["zgi"] = gyroZ;    // Giroscopio Z
 
   String jsonData;
   serializeJson(jsonDoc, jsonData);
 
   Serial.print("JSON data: ");
   Serial.println(jsonData);
-
 
   http.begin(url.c_str());
   http.addHeader("Content-Type", "application/json");
@@ -68,6 +75,46 @@ void readAndSendAllSensors() {
   }
 }
 
+// Callback que se activa cuando llega un mensaje MQTT
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Mensaje recibido en el topic ");
+  Serial.print(topic);
+  Serial.print(": ");
+  
+  String message;
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  Serial.println(message);
+  
+  // Ejecutar medición basada en el mensaje recibido
+  if (message == "m") {
+    Serial.println("Iniciando medición de todos los sensores");
+    readAndSendAllSensors();
+  } else {
+    Serial.println("Comando no reconocido");
+  }
+}
+
+// Conectar al broker MQTT
+void connectToMQTTBroker() {
+  mqttClient.setServer(mqttServer, mqttPort);
+  mqttClient.setCallback(callback);
+  
+  while (!mqttClient.connected()) {
+    Serial.println("Intentando conectar al servidor MQTT...");
+    if (mqttClient.connect(clientName)) {
+      Serial.println("Conectado al servidor MQTT!");
+      mqttClient.subscribe(topic); 
+      Serial.println("Suscrito al topic icesitel/medicion");
+    } else {
+      Serial.print("Error al conectar: ");
+      Serial.println(mqttClient.state());
+      delay(5000);
+    }
+  }
+}
+
 // Inicializar la ESP32
 void setup() {
   Serial.begin(115200);
@@ -81,6 +128,7 @@ void setup() {
   Serial.println("MPU6050 inicializado correctamente");
 
   runWiFi();
+  connectToMQTTBroker();  
 
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
@@ -89,18 +137,9 @@ void setup() {
 
 // Loop principal del programa
 void loop() {
-    if (WiFi.status() != WL_CONNECTED) {
+  if (WiFi.status() != WL_CONNECTED) {
     Serial.println("Conexión WiFi perdida. Intentando reconectar...");
     runWiFi();
   }
-}
-
-// Detectar eventos del puerto serie
-void serialEvent() {
-  if (Serial.available() > 0) {
-    String data = Serial.readStringUntil('\n');
-    if (data == "m") {
-      readAndSendAllSensors();
-    }
-  }
+  mqttClient.loop(); 
 }
